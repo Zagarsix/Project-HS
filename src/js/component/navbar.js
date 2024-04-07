2f4f75668c4fcfb68fe77ae20fd9109d9c9967b5
Zagarsix <claudk1006@gmail.com>
2024-04-06 23:54:41 -0400

    editing navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,54 +3,53 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return <>{/* Navigation*/}
-    <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
-        <div className="container px-4 px-lg-5">
-            <NavLink className="navbar-brand" to="/">
-                HomeSchool Project (logo)
-            </NavLink>
-            <button
-                className="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarResponsive"
-                aria-controls="navbarResponsive"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-            >
-                Menu
-                <i className="fas fa-bars" />
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-                <ul className="navbar-nav ms-auto py-4 py-lg-0">
-                    <li className="nav-item">
-                        <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/">
-                            Inicio
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/nosotros">
-                            Nosotros
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/matriculas">
-                            Matrículas
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/blog">
-                            Blog
-                        </NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/contacto">
-                            Contacto
-                        </NavLink>
-                    </li>
-                </ul>
+        <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
+            <div className="container px-4 px-lg-5">
+                <NavLink className="navbar-brand" to="/">
+                    Logo HomeSchool
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarResponsive"
+                    aria-controls="navbarResponsive"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <i className="fas fa-bars" />
+                </button>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto py-4 py-lg-0">
+                        <li className="nav-item">
+                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/">
+                                Inicio
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/nosotros">
+                                Nosotros
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/matriculas">
+                                Matrículas
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/blog">
+                                Blog
+                            </NavLink>
+                        </li>
+                        <li className="nav-item">
+                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/contacto">
+                                Contacto
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </nav>
+        </nav>
     </>
 }
 
