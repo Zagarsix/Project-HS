3d5d16579d59f8bdd3da15532faa46582b07afec
Zagarsix <claudk1006@gmail.com>
2024-04-08 23:57:02 -0400

    adding logo in <head>

--- webpack.common.js
+++ webpack.common.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-        favicon: 'iconDark.jpg',
+        favicon: 'iconoHS.jpg',
         template: 'template.html'
     }),
     new Dotenv({ safe: true, systemvars: true })
