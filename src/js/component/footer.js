93af7049a6d0fcb124876f5ad45e2b9a967eae97
Zagarsix <claudk1006@gmail.com>
2024-04-09 23:43:48 -0400

    editing Footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import metroLogo from "../../img/metroLogo.png";
+import iconoHS from "../../img/iconoHS.jpg"
 
 const Footer = () => {
     return <>
@@ -8,38 +9,38 @@ const Footer = () => {
         <footer className="footer py-2 mt-auto text-black">
             <div className="container px-5">
                 <div className="row align-items-center justify-content-between justify-items-end flex-column flex-sm-row">
-                    <div className='col-auto text-center p-3'>
+                    <div className='col-auto text-center p-3 m-3'>
                         <span className='m-auto mx-3'>
                             <a className='text-white' target="_blank" href="https://www.linkedin.com/in/daniela-pacheco-araya-716a32264/">
-                                <i className="fa-brands fa-linkedin fa-beat fa-2xl"></i>
+                                <i className="fa-brands fa-facebook-f fa-bounce fa-2xl" style={{ color: "blue" }}></i>
                             </a>
                         </span>
                         <span className='m-auto mx-3'>
                             <a className='text-white' target="_blank" href="https://www.instagram.com/danydent_odontoesthetic/">
-                                <i className="fa-brands fa-square-instagram fa-beat fa-2xl"></i>
+                                <i className="fa-brands fa-instagram fa-bounce fa-2xl" style={{ color: "pink" }}></i>
                             </a>
                         </span>
                         <span className='m-auto mx-3'>
                             <a className='text-white' target="_blank" href="https://wa.me/56973655180/">
-                                <i className="fa-brands fa-square-whatsapp fa-beat-fade fa-2xl "></i>
+                                <i className="fa-brands fa-whatsapp fa-bounce fa-2xl " style={{ color: "green" }}></i>
                             </a>
                         </span>
                         <span className='m-auto mx-3'>
                             <a className='text-white' target="_blank" href="mailto:dany.pacheco.a@gmail.com">
-                                <i className="fa-solid fa-square-envelope fa-beat-fade fa-2xl"></i>
+                                <i className="fa-regular fa-envelope fa-bounce fa-2xl" style={{ color: "red" }}></i>
                             </a>
                         </span>
                     </div>
-                    <div className='col-auto'>
-                        <div className="m-0 text-black text-center">
-                            <img className="mx-1" src="" alt="logo-brand" width="150" height="150"></img>
-                            <p>© Dra. Dany 2024. Todos los derechos reservados.</p>
-                            <p>Av. Nueva Providencia 1881, Providencia, Santiago, Chile.</p>
-                            <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25"/>Metro Pedro de Valdivia, salida Sur.</p>
+                    <div className='col-auto p-2 m-2'>
+                        <div className="text-white text-center">
+                            <img className="mx-1" src={iconoHS} alt="logo-brand" width="150" height="150"></img>
+                            <p>© El Pacto Homeschool 2024. Todos los derechos reservados.</p>
+                            <p>Curacaví, Estación Central, Santiago, Chile.</p>
+                            <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25" />Metro Las Rejas</p>
                             <NavLink className="link-light small" to="#!">
                                 Política de Privacidad
                             </NavLink>
-                            <span className="text-black mx-1">·</span>
+                            <span className="text-white mx-1">·</span>
                             <NavLink className="link-light small" to="#!">
                                 Términos y condiciones
                             </NavLink>
@@ -47,8 +48,7 @@ const Footer = () => {
                     </div>
                     <div className='col-auto text-center p-3'>
                         <div className='small text-center mt-3 justify-items-end'>
-                            
-                            <span className='small m-0 text-black'> &copy; 2024 - Developed with <i className="fa fa-heart text-danger" /> by {""}
+                            <span className='small m-0 text-white'> &copy; 2024 - Developed with <i className="fa fa-heart text-danger" /> by {""}
                                 <a target="_blank" href="https://github.com/Zagarsix">Zagarsix</a>
                             </span>
                         </div>
