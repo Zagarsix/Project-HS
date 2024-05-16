433f5c3bcd8e6eb687e26a7ba8b0aab0a4d66eb4
Zagarsix <claudk1006@gmail.com>
2024-05-15 23:57:59 -0400

    restyled WhatsApp button and creation of tootltip linked to the button

--- src/js/component/buttonWame.js
+++ src/js/component/buttonWame.js
@@ -2,17 +2,26 @@ import React from 'react';
 import contact from "../../img/what5.png";
 
 const ButtonWame = () => {
-  return <>
-    <div className='container fixed-bottom p-1 my-1' id="buttonWame">
-      <span className='m-auto mx-0'>
-        <a className='text-white' target="_blank" href="https://wa.me/56973655180/">
-          <span className='tooltipWame'>¿Necesitas ayuda?</span>
-          <img className="Wame p-0" src={contact} alt="logo-brand" width="75" height="75" />
-        </a>
-      </span>
-    </div>
-  </>
-
+  return (
+    <>
+      <div className='container fixed-bottom p-1 my-1' id="buttonWame">
+        <span className='d-grid gap-2 d-md-flex justify-content-md-end m-auto mx-0'>
+          <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://wa.me/56973655180/">
+            <div className="tooltip-container">
+              <img
+                className="Wame p-0"
+                src={contact}
+                alt="logo-brand"
+                width="75"
+                height="75"
+              />
+              <div className="tooltip-text">¿Necesitas ayuda?</div>
+            </div>
+          </a>
+        </span>
+      </div>
+    </>
+  );
 }
 
 export default ButtonWame;
