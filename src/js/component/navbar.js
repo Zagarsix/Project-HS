c2ebaaedf85e602b5dd6e3a55a028ab7d714ddbf
Zagarsix <claudk1006@gmail.com>
2024-05-16 03:45:06 -0400

    set a collapsible navigation bar when selecting a view

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,57 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
 import logoHS from "../../img/Elpacto-bg34.png";
 
 const Navbar = () => {
-    return <>{/* Navigation*/}
-        <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
-            <div className="container px-4 px-lg-5">
-                <NavLink className="navbar-brand" to="/">
-                <img className="mx-3" src={logoHS} alt="logo-brand" width="220" height="120" />
-                </NavLink>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarResponsive"
-                    aria-controls="navbarResponsive"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <i className="fas fa-bars" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbarResponsive">
-                    <ul className="navbar-nav ms-auto py-4 py-lg-0">
-                        <li className="nav-item">
-                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/">
-                                Inicio
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/nosotros">
-                                Nosotros
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/matriculas">
-                                Matrículas
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/blog">
-                                Blog
-                            </NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/contacto">
-                                Contacto
-                            </NavLink>
-                        </li>
-                    </ul>
+    const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+    const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
+    const navCollapseClass = isNavCollapsed ? 'collapse' : '';
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light" id="mainNav">
+                <div className="container px-4 px-lg-5">
+                    <NavLink className="navbar-brand" to="/">
+                        <img className="mx-3" src={logoHS} alt="logo-brand" width="220" height="120" />
+                    </NavLink>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarResponsive"
+                        aria-controls="navbarResponsive"
+                        aria-expanded={!isNavCollapsed ? true : false}
+                        aria-label="Toggle navigation"
+                        onClick={handleNavCollapse}
+                    >
+                        <i className="fas fa-bars" />
+                    </button>
+                    <div className={`${navCollapseClass} navbar-collapse`} id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto py-4 py-lg-0">
+                            <li className="nav-item">
+                                <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/" onClick={handleNavCollapse}>
+                                    Inicio
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/nosotros" onClick={handleNavCollapse}>
+                                    Nosotros
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/matriculas" onClick={handleNavCollapse}>
+                                    Matrículas
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/blog" onClick={handleNavCollapse}>
+                                    Blog
+                                </NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link px-lg-3 py-3 py-lg-4" to="/contacto" onClick={handleNavCollapse}>
+                                    Contacto
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    </>
+            </nav>
+        </>
+    );
 }
 
 export default Navbar;
