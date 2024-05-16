aba3009b5f943e648ba20971c29f74169bc3ef7f
Zagarsix <claudk1006@gmail.com>
2024-05-16 01:39:56 -0400

    edit position, margins and padding of the WhatsApp button and its tooltip

--- src/js/component/buttonWame.js
+++ src/js/component/buttonWame.js
@@ -3,24 +3,20 @@ import contact from "../../img/what5.png";
 
 const ButtonWame = () => {
   return (
-    <>
-      <div className='container fixed-bottom p-1 my-1' id="buttonWame">
-        <span className='d-grid gap-2 d-md-flex justify-content-md-end m-auto mx-0'>
-          <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://wa.me/56973655180/">
-            <div className="tooltip-container">
-              <img
-                className="Wame p-0"
-                src={contact}
-                alt="logo-brand"
-                width="75"
-                height="75"
-              />
-              <div className="tooltip-text">¿Necesitas ayuda?</div>
-            </div>
-          </a>
-        </span>
+    <div className="fixed-button-container">
+      <div className="tooltip-container">
+        <a className="fixed-button" target="_blank" rel="noopener noreferrer" href="https://wa.me/56973655180/">
+          <img
+            className='Wame'
+            src={contact}
+            alt="logo-brand"
+            width="60"
+            height="60"
+          />
+        </a>
+        <div className="tooltip-text">¿Necesitas ayuda?</div>
       </div>
-    </>
+    </div>
   );
 }
 
