11a1c568b4a51c6f251b7d0d1e1ecab95424d1aa
Zagarsix <claudk1006@gmail.com>
2024-05-16 07:56:35 -0400

    edit margins and padding in Footer (responsive)

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -4,39 +4,38 @@ import metroLogo from "../../img/metroLogo.png";
 import iconoHS from "../../img/ElpactoDarkicon.png"
 
 const Footer = () => {
-    return <>
-        {/* Footer*/}
+    return (
         <footer className="footer py-2 mt-auto text-black">
-            <div className="container px-5">
-                <div className="row align-items-center justify-content-between justify-items-end flex-column flex-sm-row">
-                    <div className='col-auto text-center p-3 m-3'>
-                        <span className='m-auto mx-3'>
-                            <a className='text-white' target="_blank" href="https://www.linkedin.com/in/daniela-pacheco-araya-716a32264/">
+            <div className="container px-4">
+                <div className="row align-items-center justify-content-between">
+                    <div className='col-12 col-sm-auto text-center p-3'>
+                        <span className='m-2'>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniela-pacheco-araya-716a32264/">
                                 <i className="fa-brands fa-facebook-f fa-bounce fa-2xl" style={{ color: "blue" }}></i>
                             </a>
                         </span>
-                        <span className='m-auto mx-3'>
-                            <a className='text-white' target="_blank" href="https://www.instagram.com/danydent_odontoesthetic/">
+                        <span className='m-2'>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/danydent_odontoesthetic/">
                                 <i className="fa-brands fa-instagram fa-bounce fa-2xl" style={{ color: "pink" }}></i>
                             </a>
                         </span>
-                        <span className='m-auto mx-3'>
-                            <a className='text-white' target="_blank" href="https://wa.me/56973655180/">
+                        <span className='m-2'>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://wa.me/56973655180/">
                                 <i className="fa-brands fa-whatsapp fa-bounce fa-2xl " style={{ color: "green" }}></i>
                             </a>
                         </span>
-                        <span className='m-auto mx-3'>
-                            <a className='text-white' target="_blank" href="mailto:dany.pacheco.a@gmail.com">
+                        <span className='m-2'>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="mailto:dany.pacheco.a@gmail.com">
                                 <i className="fa-regular fa-envelope fa-bounce fa-2xl" style={{ color: "red" }}></i>
                             </a>
                         </span>
                     </div>
-                    <div className='col-auto p-2 m-2'>
-                        <div className="text-white text-center">
-                            <img className="mx-1" src={iconoHS} alt="logo-brand" width="120" height="100"></img>
+                    <div className='col-12 col-sm-auto p-2 m-2 text-center'>
+                        <div className="text-white">
+                            <img className="mx-1" src={iconoHS} alt="logo-brand" width="120" height="100" />
                             <p>© El Pacto Homeschool 2024. Todos los derechos reservados.</p>
                             <p>Curacaví, Estación Central, Santiago, Chile.</p>
-                            <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25" />Metro Las Rejas</p>
+                            <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25" /> Metro Las Rejas</p>
                             <NavLink className="link-light small" to="#!">
                                 Política de Privacidad
                             </NavLink>
@@ -46,9 +45,9 @@ const Footer = () => {
                             </NavLink>
                         </div>
                     </div>
-                    <div className='col-auto text-center p-3'>
-                        <div className='small text-center mt-3 justify-items-end'>
-                            <span className='small m-0 text-white'> &copy; 2024 - Developed with <i className="fa fa-heart text-danger" /> by {""}
+                    <div className='col-12 col-sm-auto text-center p-3'>
+                        <div className='small mt-3'>
+                            <span className='text-white'> &copy; 2024 - Developed with <i className="fa fa-heart text-danger" /> by{" "}
                                 <a target="_blank" href="https://github.com/Zagarsix">Zagarsix</a>
                             </span>
                         </div>
@@ -56,7 +55,7 @@ const Footer = () => {
                 </div>
             </div>
         </footer>
-    </>
+    )
 }
 
-export default Footer; 
+export default Footer;
