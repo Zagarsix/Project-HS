e8556ed770543efdb0456e9441b9f90d0528140c
Zagarsix <claudk1006@gmail.com>
2024-05-23 23:51:41 -0400

    icons fix

--- src/js/views/contacto.js
+++ src/js/views/contacto.js
@@ -32,7 +32,7 @@ return<>
         <div className="bg-light rounded-4 py-5 px-4 px-md-5 mb-5" id="contacto">
           <div className="text-center mb-5">
             <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
-              <i className="bi bi-envelope" />
+              {/* <i className="bi bi-envelope" /> */}
             </div>
             <h1 className="fw-bolder">Contáctanos</h1>
             <p className="lead fw-normal text-muted mb-0">
