cc72dcc79270c1683a26eff20e6c8a94ba54cf63
Zagarsix <claudk1006@gmail.com>
2024-07-02 23:55:59 -0400

    add content in footer.js

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,57 +1,58 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
 import metroLogo from "../../img/metroLogo.png";
-import iconoHS from "../../img/logo-fondo-oscuro.png"
+import logoDark from "../../img/logo-fondo-oscuro.png"
 
 const Footer = () => {
     return (
-        <footer className="footer py-2 mt-auto text-black">
-            <div className="container px-4">
-                <div className="row align-items-center justify-content-between">
-                    <div className='col-12 col-sm-auto text-center p-3'>
+        <footer className="footer mt-auto">
+            <div className="container p-1">
+                <div className="row align-items-center justify-content-between my-5 p-1">
+                    {/*Logo y lema*/}
+                    <div className='col-12 col-md-5 text-start p-3 mb-4 order-1 order-md-1'>
+                        <img className="mx-1" src={logoDark} alt="logo-brand" width="270" height="120" />
+                        <p className='lema'> «...Yo hago un pacto con ustedes y les prometo que mi Espíritu y las enseñanzas que les he dado, no se apartarán jamás de ustedes ni de sus hijos...»
+                        </p>
+                    </div>
+                    {/*Contacto*/}
+                    <div className='col-12 col-md-4 text-start p-4 mb-5 order-2 order-md-3'>
+                        <h4>Contacto</h4>
+                        <p>- cvalianza@gmail.com</p>
+                        <p>- +569 28484497</p>
+                        <p>- Providencia, Santiago, Chile.</p>
+                        <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25" /> Metro Pedro de Valdivia</p>
+                        <NavLink className="link-light small" to="#!">
+                            Política de Privacidad
+                        </NavLink>
+                        <span className="text-white mx-1">·</span>
+                        <NavLink className="link-light small" to="#!">
+                            Términos y condiciones
+                        </NavLink>
+                    </div>
+                    {/*Redes sociales*/}
+                    <div className='col-12 col-md-3 text-center p-1 mb-4 mt-4 order-3 order-md-2'>
                         <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/daniela-pacheco-araya-716a32264/">
-                                <i className="fa-brands fa-facebook-f fa-fade fa-2xl" style={{ color: "blue" }}></i>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.facebook.com">
+                                <i className="fa-brands fa-square-facebook fa-fade fa-2xl" style={{ color: "blue" }}></i>
                             </a>
                         </span>
                         <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/danydent_odontoesthetic/">
-                                <i className="fa-brands fa-instagram fa-fade fa-2xl" style={{ color: "pink" }}></i>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/">
+                                <i className="fa-brands fa-square-instagram fa-fade fa-2xl" style={{ color: "purple" }}></i>
                             </a>
                         </span>
                         <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://wa.me/56973655180/">
-                                <i className="fa-brands fa-whatsapp fa-fade fa-2xl " style={{ color: "green" }}></i>
-                            </a>
-                        </span>
-                        <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="mailto:dany.pacheco.a@gmail.com">
-                                <i className="fa-regular fa-envelope fa-fade fa-2xl" style={{ color: "red" }}></i>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/">
+                                <i className="fa-brands fa-square-twitter fa-fade fa-2xl" style={{ color: "#0d6efd" }}></i>
                             </a>
                         </span>
                     </div>
-                    <div className='col-12 col-sm-auto p-2 m-2 text-center'>
-                        <div className="text-white">
-                            <img className="mx-1" src={iconoHS} alt="logo-brand" width="180" height="80" />
-                            <p>© El Pacto Homeschool 2024. Todos los derechos reservados.</p>
-                            <p>Curacaví, Estación Central, Santiago, Chile.</p>
-                            <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25" /> Metro Las Rejas</p>
-                            <NavLink className="link-light small" to="#!">
-                                Política de Privacidad
-                            </NavLink>
-                            <span className="text-white mx-1">·</span>
-                            <NavLink className="link-light small" to="#!">
-                                Términos y condiciones
-                            </NavLink>
-                        </div>
-                    </div>
-                    <div className='col-12 col-sm-auto text-center p-3'>
-                        <div className='small mt-3'>
-                            <span className='text-white'> &copy; 2024 - Developed with <i className="fa fa-heart text-danger" /> by{" "}
-                                <a target="_blank" href="https://github.com/Zagarsix">Zagarsix</a>
-                            </span>
-                        </div>
-                    </div>
+                </div>
+                <div className='down col-12 text-center p-2'>
+                    <p>© Colegio Virtual Alianza 2024. Todos los derechos reservados.</p>
+                    <span className='text-white'>Developed with <i className="fa fa-heart text-danger" /> by{" "}
+                        <a className="developer" target="_blank" href="https://github.com/Zagarsix">Zagarsix</a>
+                    </span>
                 </div>
             </div>
         </footer>
