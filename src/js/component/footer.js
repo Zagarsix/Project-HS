4ba1da67dcb7e9dfe3e446300b6a0abcf5e11435
Zagarsix <claudk1006@gmail.com>
2024-07-21 20:12:59 -0400

    update social media content in footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -9,17 +9,17 @@ const Footer = () => {
             <div className="container p-1">
                 <div className="row align-items-center justify-content-between my-5 p-1">
                     {/*Logo y lema*/}
-                    <div className='col-12 col-md-5 text-start p-3 mb-4 order-1 order-md-1'>
+                    <div className='col-12 col-md-4 text-start p-1 mb-4 order-1 order-md-1'>
                         <img className="mx-1" src={logoDark} alt="logo-brand" width="270" height="120" />
                         <p className='lema'> «...Yo hago un pacto con ustedes y les prometo que mi Espíritu y las enseñanzas que les he dado, no se apartarán jamás de ustedes ni de sus hijos...»
                         </p>
                     </div>
                     {/*Contacto*/}
-                    <div className='col-12 col-md-4 text-start p-4 mb-5 order-2 order-md-3'>
+                    <div className='col-12 col-md-4 text-start p-1 mb-4 order-2 order-md-3'>
                         <h4>Contacto</h4>
-                        <p>- cvalianza@gmail.com</p>
-                        <p>- +569 28484497</p>
-                        <p>- Providencia, Santiago, Chile.</p>
+                        <p>colegiovirtualalianza.adm@gmail.com</p>
+                        <p>+569 28484497</p>
+                        <p>Providencia, Santiago, Chile.</p>
                         <p><img className="mx-0" src={metroLogo} alt="Metro Logo" width="30" height="25" /> Metro Pedro de Valdivia</p>
                         <NavLink className="link-light small" to="#!">
                             Política de Privacidad
@@ -30,29 +30,36 @@ const Footer = () => {
                         </NavLink>
                     </div>
                     {/*Redes sociales*/}
-                    <div className='col-12 col-md-3 text-center p-1 mb-4 mt-4 order-3 order-md-2'>
+                    <div className='col-12 col-md-4 text-center p-1 mb-4 mt-4 order-3 order-md-2'>
+                    <h4 className='mb-3'>Síguenos</h4>
                         <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.facebook.com">
-                                <i className="fa-brands fa-square-facebook fa-fade fa-2xl" style={{ color: "blue" }}></i>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/profile.php?id=61562070452384">
+                            <div className='space-facebook'>
+                            <i className="fa-brands fa-facebook-f mt-2"></i>
+                            </div>
                             </a>
                         </span>
                         <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/">
-                                <i className="fa-brands fa-square-instagram fa-fade fa-2xl" style={{ color: "purple" }}></i>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/alianza.homeschool/">
+                            <div className='space-instagram'>
+                            <i className="fa-brands fa-instagram mt-2"></i>
+                            </div>
                             </a>
                         </span>
                         <span className='m-2'>
-                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.twitter.com/">
-                                <i className="fa-brands fa-square-twitter fa-fade fa-2xl" style={{ color: "#0d6efd" }}></i>
+                            <a className='text-white' target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/@colegio_virtual_alianza">
+                            <div className='space-tiktok'>
+                            <i className="fa-brands fa-tiktok mt-2"></i>
+                            </div>
                             </a>
                         </span>
                     </div>
                 </div>
-                <div className='down col-12 text-center p-2'>
-                    <p>© Colegio Virtual Alianza 2024. Todos los derechos reservados.</p>
-                    <span className='text-white'>Developed with <i className="fa fa-heart text-danger" /> by{" "}
+                <div className='down col-12 col-xs-12 text-center py-2'>
+                    <span>© Colegio Virtual Alianza 2024.</span><span> Todos los derechos reservados.</span>
+                    <p className='text-white'>Developed with <i className="fa fa-heart text-danger" /> by{" "}
                         <a className="developer" target="_blank" href="https://github.com/Zagarsix">Zagarsix</a>
-                    </span>
+                    </p>
                 </div>
             </div>
         </footer>
