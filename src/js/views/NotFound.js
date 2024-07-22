4ed6122ead34c9f213d48d777b39b009c01c0b4e
Zagarsix <claudk1006@gmail.com>
2024-07-21 20:14:18 -0400

    fix margins and content of view NotFound

--- src/js/views/NotFound.js
+++ src/js/views/NotFound.js
@@ -9,7 +9,7 @@ const NotFound = () => {
                 <span style={{color:"#ff8800"}}>Error 404: </span> <span style={{ color: "#0e408f" }}> Página no disponible.</span>
             </h1>
             <div className='row justify-content-center'>
-                <div className="py-3 px-4 px-md-5 mb-3 col-lg-6 col-xl-6 order-2 order-md-1">
+                <div className="py-3 px-4 px-md-5 mb-3 col-lg-6 col-xl-7 order-2 order-md-1">
                     <h2 className="h5 m-3"><i className="fa-regular fa-face-surprise fa-2x p-1" /> ¡Ups! ¿Te has equivocado de enlace?</h2>
                     <p className="m-3">
                         No hemos encontrado la página que estás buscando.<br />
@@ -22,7 +22,7 @@ const NotFound = () => {
                     </Link>
                 </div>
                 {/* Illustration*/}
-                <div className='col-lg-6 col-xl-6 mt-3 py-2 px-4 order-md-2 order-1'>
+                <div className='col-lg-6 col-xl-5 mt-3 py-2 px-4 order-md-2 order-1'>
                     <img
                         className="img-fluid mb-2 px-2"
                         width="250"
