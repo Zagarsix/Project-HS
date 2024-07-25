bc65a9790488ec9721967f7019649d8179f5920c
Zagarsix <claudk1006@gmail.com>
2024-07-24 23:59:51 -0400

    import carousel and update content in testimonials section in home.js

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -7,6 +7,8 @@ import chave from "../../img/chave3.jpg";
 import children from "../../img/childrenSchool.jpg";
 import notebook from "../../img/about-bg.jpg";
 import parents from "../../img/ParentsAndChildren.jpg";
+import social from "../../img/social-media.png";
+import TestimonialCarousel from "../component/carousel";
 
 
 const Home = () => {
@@ -19,10 +21,10 @@ const Home = () => {
           <div className="row gx-4 gx-lg-5 justify-content-start">
             <div className="col-md-12 col-lg-12 col-xl-10">
               <div className="site-heading">
-                <h1 id="specialName" style={{fontWeight:"bolder", color:"white"}}>Educación &nbsp;</h1>
-                <h2 className="subtittle" style={{color: "white", fontWeight: "bold", fontStyle:"italic"}}>
+                <h1 id="specialName" style={{ fontWeight: "bolder", color: "white" }}>Educación &nbsp;</h1>
+                <h2 className="subtittle" style={{ color: "white", fontWeight: "bold", fontStyle: "italic" }}>
                   <Typewriter
-                    words={['online!', 'cristiana!', 'personalizada!', 'flexible!','innovadora!','de calidad!']}
+                    words={['online!', 'cristiana!', 'personalizada!', 'flexible!', 'innovadora!', 'de calidad!']}
                     loop cursor delaySpeed={2000}
                     cursorStyle='|'
                   /></h2>
@@ -37,7 +39,7 @@ const Home = () => {
         <div className="container px-4 px-lg-5 text-center">
           <div className="row gx-4 gx-lg-5 justify-content-center">
             <div className="col-lg-10">
-              <h3><span style={{ color: "#ff8800" }}>Colegio Virtual</span><span style={{ fontWeight:"bolder", color:"#0e408f" }}> Alianza</span> es el lugar perfecto para que tus hijos den sus próximos pasos!</h3>
+              <h3><span style={{ color: "#ff8800" }}>Colegio Virtual</span><span style={{ fontWeight: "bolder", color: "#0e408f" }}> Alianza</span> es el lugar perfecto para que tus hijos den sus próximos pasos!</h3>
               <p className="lead mb-2">
                 Experimenta esta nueva modalidad de estudio <strong>b-learning:</strong> clases online y talleres presenciales. No los dejes sin su&nbsp;
                 <Link className='link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/matriculas">matrícula!</Link>
@@ -57,10 +59,10 @@ const Home = () => {
         <div className="containerServices px-4 px-lg-3 mt-2 mb-0">
           <div className="content-section-heading mb-5">
             <h2 className="mb-2">Bienvenidos a
-            <span className="mb-1" style={{ color: "#ff8800" }}> Alianza</span></h2>
+              <span className="mb-1" style={{ color: "#ff8800" }}> Alianza</span></h2>
             <p> Somos la alternativa ideal a la educación tradicional. Ofrecemos un enfoque innovador basado en la modalidad de Homeschooling, diseñado para brindar una experiencia educativa flexible, personalizada y en armonía con las necesidades de nuestros estudiantes y sus familias.</p>
           </div>
-          <div className="row gx-4 gx-lg-5 mt-5" style={{color:"#3dfffb"}}>
+          <div className="row gx-4 gx-lg-5 mt-5" style={{ color: "#3dfffb" }}>
             <div className="col-lg-3 col-md-6 mb-5 mb-lg-0">
               <span className="service-icon rounded-circle mx-auto mb-3">
                 <i className="fa-solid fa-id-card fa-2xl" />
@@ -87,7 +89,7 @@ const Home = () => {
                 <span>Formación Integral</span>
               </h4>
               <p className="text-white-50 mb-0">
-              Más tiempo para explorar intereses personales y desarrollar habilidades en áreas como deportes, arte y otras actividades extracurriculares.
+                Más tiempo para explorar intereses personales y desarrollar habilidades en áreas como deportes, arte y otras actividades extracurriculares.
               </p>
             </div>
             <div className="col-lg-3 col-md-6 mb-5 mb-lg-0">
@@ -107,8 +109,13 @@ const Home = () => {
         <div className="container px-5 my-5">
           <div className="row gx-5">
             <div className="col-lg-4 mb-5 mb-lg-0">
-              <i className="fa-solid fa-ranking-star fa-5x mb-2" style={{ color: "#ff8800" }} />
-              <h2 className="fw-bolder mb-0" style={{ color: "#0e408f" }}>Marcando la diferencia:</h2>
+              <img
+                className="img-fluid mb-2 px-2"
+                width="250"
+                height="250"
+                src={social}
+                alt="ilustración social media"
+              />
             </div>
             <div className="col-lg-8">
               <div className="row gx-5 row-cols-1 row-cols-md-2">
@@ -116,7 +123,7 @@ const Home = () => {
                   <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                     {/* <i className="bi bi-collection" /> */}
                   </div>
-                  <h2 className="h5"><i className="fa-solid fa-magnifying-glass-chart fa-2xl" style={{ color: "#0e408f" }} /> Innovación pedagógica</h2>
+                  <h2 className="h5"><i className="fa-solid fa-magnifying-glass-chart fa-2xl p-1 m-1" style={{ color: "#0e408f" }} /> Innovación pedagógica</h2>
                   <p className="mb-0">
                     Utilizamos las últimas tecnologías y metodologías educativas para ofrecer una experiencia de aprendizaje dinámica e interactiva.<br />
                     <Link className='link-info link-offset-2 link-underline-opacity-75 link-underline-opacity-100-hover' to="/contacto"> Leer más...</Link>
@@ -126,7 +133,7 @@ const Home = () => {
                   <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                     {/* <i className="bi bi-building" /> */}
                   </div>
-                  <h2 className="h5"><i className="fa-solid fa-hand-holding-dollar fa-2xl" style={{ color: "#0e408f" }} /> Métodos de pago</h2>
+                  <h2 className="h5"><i className="fa-solid fa-hand-holding-dollar fa-2xl p-1 m-1" style={{ color: "#0e408f" }} /> Métodos de pago</h2>
                   <p className="mb-0">
                     Contamos con todo medio de pago: tarjeta de crédito, transferencia y efectivo.<br />
                     <Link className='link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/matriculas"> Leer más...</Link>
@@ -136,16 +143,16 @@ const Home = () => {
                   <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                     {/* <i className="bi bi-toggles2" /> */}
                   </div>
-                  <h2 className="h5"><i className="fa-solid fa-briefcase-medical fa-2xl" style={{ color: "#0e408f" }} /> Docencia de alto nivel</h2>
+                  <h2 className="h5"><i className="fa-solid fa-briefcase-medical fa-2xl p-1 m-1" style={{ color: "#0e408f" }} /> Docencia de alto nivel</h2>
                   <p className="mb-0">
-                  Clases online y presencial. Contempla los contenidos  y malla curricular del Mineduc.<br />
+                    Clases online y presencial. Contempla los contenidos  y malla curricular del Mineduc.<br />
                     <Link className='link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/nosotros"> Leer más...</Link>
                   </p>
                 </div>
                 <div className="col h-100">
                   <div className="feature bg-primary bg-gradient text-white rounded-3 mb-3">
                   </div>
-                  <h2 className="h5"><i className="fa-solid fa-user-doctor fa-2xl" style={{ color: "#0e408f" }} /> Talleres complementarios</h2>
+                  <h2 className="h5"><i className="fa-solid fa-user-doctor fa-2xl p-1 m-1" style={{ color: "#0e408f" }} /> Talleres complementarios</h2>
                   <p className="mb-0">
                     Paragraph of text beneath the heading to explain the heading.<br />
                     <Link className='link-info link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover' to="/blog"> Leer más...</Link>
@@ -158,71 +165,8 @@ const Home = () => {
       </section>
       {/* Testimonial section*/}
       <div className="py-2" id="testimonial">
-        <div className="container px-5 my-3 text-white">
-          <div className="row gx-5 justify-content-center">
-            <div className="col-lg-10 col-xl-7">
-              <div className="text-center">
-                <div className="fs-6 mb-4 fst-italic">
-                  "Trabajando con Homeschool El Pacto ha sido de bendición para el desarrollo de....
-                  resto de palabras que no ha dicho, pero que podrá decir, para sacar adelante este Proyecto!"
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                  <img
-                    className="rounded-circle me-3"
-                    src={chave}
-                    alt="Apóstol Isabel riendo"
-                    width="120"
-                    height="120"
-                  />
-                  <div className="text-white-50 fw-bold">
-                    Isabel Fernández
-                    <span className="fw-bold text-primary mx-1">/</span>
-                    Apóstol MCA
-                  </div>
-                </div>
-              </div>
-              <div className="text-center">
-                <div className="fs-6 mb-4 fst-italic">
-                  "Trabajando con Homeschool El Pacto ha sido de bendición para el desarrollo de....
-                  resto de palabras que no ha dicho, pero que podrá decir, para sacar adelante este Proyecto!"
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                  <img
-                    className="rounded-circle me-3"
-                    src={chave}
-                    alt="Apóstol Isabel riendo"
-                    width="120"
-                    height="120"
-                  />
-                  <div className="text-white-50 fw-bold">
-                    Isabel Fernández
-                    <span className="fw-bold text-primary mx-1">/</span>
-                    Apóstol MCA
-                  </div>
-                </div>
-              </div>
-              <div className="text-center">
-                <div className="fs-6 mb-4 fst-italic">
-                  "Trabajando con Homeschool El Pacto ha sido de bendición para el desarrollo de....
-                  resto de palabras que no ha dicho, pero que podrá decir, para sacar adelante este Proyecto!"
-                </div>
-                <div className="d-flex align-items-center justify-content-center">
-                  <img
-                    className="rounded-circle me-3"
-                    src={chave}
-                    alt="Apóstol Isabel riendo"
-                    width="120"
-                    height="120"
-                  />
-                  <div className="text-white-50 fw-bold">
-                    Isabel Fernández
-                    <span className="fw-bold text-primary mx-1">/</span>
-                    Apóstol MCA
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+        <div className="container px-5 my-3 text-black">
+          <TestimonialCarousel />
         </div>
       </div>
       {/* Blog preview section*/}
