c305965e0613443e925016c6f04878273bcbbe15
Zagarsix <claudk1006@gmail.com>
2024-07-29 03:12:29 -0400

    fix padding and margin of component carousel.js

--- src/js/component/carousel.js
+++ src/js/component/carousel.js
@@ -8,11 +8,11 @@ import mom2 from "../../img/mom2.jpg";
 import mom3 from "../../img/mom3.jpg";
 
 const testimonials = [
-  { name: 'Juan Inostroza', message: 'Este colegio ha cambiado la vida de mis hijos para mejor.', role: 'Padre de familia', image: dad1 },
+  { name: 'Juan Inostroza', message: 'Sinceramente, este colegio ha cambiado la vida de mis hijos para mejor.', role: 'Padre de familia', image: dad1 },
   { name: 'María Gómez', message: 'El enfoque personalizado ha permitido que mi hijo aprenda a su propio ritmo.', role: 'Madre de familia', image: mom1 },
-  { name: 'Luis Rodríguez', message: 'Los profesores están siempre disponibles y son muy profesionales.', role: 'Padre de familia', image: dad2 },
-  { name: 'Ana López', message: 'Las clases virtuales son interactivas y muy bien estructuradas.', role: 'Madre de familia',image: mom2 },
-  { name: 'Carlos Fernández', message: 'Mis hijos disfrutan aprendiendo desde casa con los recursos que proporciona el colegio.', role: 'Padre de familia', image: dad3 },
+  { name: 'Luis Rodríguez', message: 'Nada que decir, los profes son muy profesionales y siempre están disponibles.', role: 'Padre de familia', image: dad2 },
+  { name: 'Ana López', message: 'Las clases virtuales son interactivas y muy bien estructuradas.', role: 'Madre de familia', image: mom2 },
+  { name: 'Carlos Fernández', message: 'Mis hijos disfrutan aprendiendo desde la casa con los recursos que entrega el colegio.', role: 'Padre de familia', image: dad3 },
   { name: 'Sofía Martínez', message: 'El programa educativo es excelente y se adapta a las necesidades de cada estudiante.', role: 'Madre de familia', image: mom3 }
 ];
 
@@ -25,19 +25,18 @@ const TestimonialCarousel = () => {
 
   return (
     <div id="testimonialCarousel" className="carousel slide" data-bs-ride="carousel">
-      <h1 className="text-center mb-5" style={{ color: "#ff8800" }}>Testimonios</h1>
       <div className="carousel-inner">
         {groupedTestimonials.map((group, index) => (
           <div className={`carousel-item ${index === 0 ? 'active' : ''}`} key={index}>
             <div className="d-flex flex-wrap justify-content-around">
               {group.map((testimonial, idx) => (
-                <div key={idx} className="testimonial-card p-1 m-2">
+                <div key={idx} className="testimonial-card px-1 mx-1">
                   <p className='fs-4 mb-5 fst-italic'>"{testimonial.message}"</p>
                   <div className='fs-6 d-flex align-item-center justify-content-center'>
-                    <img src={testimonial.image} alt={testimonial.name} className="rounded-circle mb-3" />
-                    <div className='fw-bold'>{testimonial.name}
-                      <span className='fw-bold text-primary mx-1'>/{testimonial.role}</span>
-                    </div>
+                    <img src={testimonial.image} alt={testimonial.name} className="rounded-circle mb-1" />
+                  </div>
+                  <div className='fw-bold' style={{fontSize:"1rem"}}>{testimonial.name}
+                    <span className='text-primary mx-1'>/{testimonial.role}</span>
                   </div>
                 </div>
               ))}
