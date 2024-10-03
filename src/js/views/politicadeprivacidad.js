3502a48c11ee70662e0fe68142b9595b7e04cb08
Zagarsix <claudk1006@gmail.com>
2024-10-03 00:00:49 -0300

    edit content of privacy policy

--- src/js/views/politicadeprivacidad.js
+++ src/js/views/politicadeprivacidad.js
@@ -18,68 +18,57 @@ const PrivacyPolicy = () => {
                         </li>
                         <li>
                             <h2 className="h5 m-3"> <strong>Información que Recogemos</strong></h2>
-                                <ul className='m-3'>
-                                    <li>Información Personal: Nombre, dirección de correo electrónico, número de teléfono, dirección física.</li>
-                                    <li>Información Académica: Historial académico, resultados de exámenes, participación en actividades.</li>
-                                    <li>Datos de Uso: Información sobre cómo accedes y usas nuestra plataforma.</li>
-                                </ul><br />
+                            <ul className='m-3'>
+                                <li><strong>Información Personal:</strong> Nombre, dirección de correo electrónico, número de teléfono, dirección física.</li>
+                                <li><strong>Información Académica:</strong> Historial académico, resultados de exámenes, participación en actividades.</li>
+                                <li><strong>Datos de Uso:</strong> Información sobre cómo accedes y usas nuestra plataforma.</li>
+                            </ul>
                         </li>
                         <li>
-                            <h2 className="h5 m-3"> <strong>Registro y Responsabilidad</strong></h2>
+                            <h2 className="h5 m-3"> <strong>Uso de la Información</strong></h2>
                             <p className="m-3">
-                                Al registrarte, te comprometes a proporcionar información veraz y actualizada. Eres responsable de mantener la confidencialidad de tus credenciales de acceso.<br />
+                                Utilizamos la información recopilada para:
+                            </p>
+                            <ul>
+                                <li>Proporcionar y gestionar nuestros servicios educativos.</li>
+                                <li>Mejorar la calidad de nuestra plataforma.</li>
+                                <li>Comunicarte sobre novedades y actualizaciones.</li>
+                                <li>Cumplir con obligaciones legales.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2 className="h5 m-3"> <strong>Protección de Datos</strong></h2>
+                            <p className="m-3">
+                                Implementamos medidas de seguridad para proteger tu información personal contra accesos no autorizados, alteraciones, divulgación o destrucción.<br />
                             </p>
                         </li>
                         <li>
-                            <h2 className="h5 m-3"> <strong>Uso Aceptable</strong></h2>
+                            <h2 className="h5 m-3"> <strong>Compartición de Información</strong></h2>
                             <p className="m-3">
-                                Te comprometes a usar nuestra plataforma de manera legal y ética, evitando comportamientos como acoso, fraude o uso de contenido protegido sin autorización.<br />
+                                No compartimos tu información personal con terceros, excepto en los siguientes casos:
+                            </p>
+                            <ul>
+                                <li>
+                                    Con tu consentimiento.</li>
+                                <li>Para cumplir con la ley o procesos legales.</li>
+                                <li>Para proteger los derechos y la seguridad de Alianza y sus usuarios.</li>
+                            </ul>
+                        </li>
+                        <li>
+                            <h2 className="h5 m-3"> <strong>Derechos de los Usuarios</strong></h2>
+                            <p className="m-3">
+                                Tienes derecho a acceder, rectificar y eliminar tu información personal. Para ejercer estos derechos, contáctanos a través de la dirección de correo electrónico proporcionada en nuestra plataforma.<br />
                             </p>
                         </li>
                         <li>
-                            <h2 className="h5 m-3"> <strong>Propiedad Intelectual</strong></h2>
+                            <h2 className="h5 m-3"> <strong>Cambios en la Política de Privacidad</strong></h2>
                             <p className="m-3">
-                                Todos los contenidos de Colegio Virtual Alianza, incluyendo materiales educativos, gráficos y marcas, están protegidos por derechos de autor y otras leyes de propiedad intelectual.<br />
-                            </p>
-                        </li>
-                        <li>
-                            <h2 className="h5 m-3"> <strong>Limitación de Responsabilidad</strong></h2>
-                            <p className="m-3">
-                                Alianza no será responsable de daños directos, indirectos, incidentales o consecuentes que puedan surgir del uso de nuestra plataforma.<br />
-                            </p>
-                        </li>
-                        <li>
-                            <h2 className="h5 m-3"> <strong>Modificaciones a los Términos</strong></h2>
-                            <p className="m-3">
-                                Nos reservamos el derecho de modificar estos términos en cualquier momento. Las modificaciones serán efectivas al momento de su publicación en nuestra plataforma.<br />
-                            </p>
-                        </li>
-                        <li>
-                            <h2 className="h5 m-3"> <strong>Ley Aplicable</strong></h2>
-                            <p className="m-3">
-                                Estos términos se rigen por la Ley General de Educación N° 20.370 de la República de Chile.<br />
+                                Nos reservamos el derecho a actualizar esta política. Notificaremos a los usuarios sobre cualquier cambio significativo.<br />
                             </p>
                         </li>
                     </ol>
                 </div>
             </div>
-
-            3. Uso de la Información Utilizamos la información recopilada para:
-
-            Proporcionar y gestionar nuestros servicios educativos.
-            Mejorar la calidad de nuestra plataforma.
-            Comunicarte sobre novedades y actualizaciones.
-            Cumplir con obligaciones legales.
-            4. Protección de Datos Implementamos medidas de seguridad para proteger tu información personal contra accesos no autorizados, alteraciones, divulgación o destrucción.
-
-            5. Compartición de Información No compartimos tu información personal con terceros, excepto en los siguientes casos:
-
-            Con tu consentimiento.
-            Para cumplir con la ley o procesos legales.
-            Para proteger los derechos y la seguridad de Alianza y sus usuarios.
-            6. Derechos de los Usuarios Tienes derecho a acceder, rectificar y eliminar tu información personal. Para ejercer estos derechos, contáctanos a través de la dirección de correo electrónico proporcionada en nuestra plataforma.
-
-            7. Cambios en la Política de Privacidad Nos reservamos el derecho a actualizar esta política. Notificaremos a los usuarios sobre cualquier cambio significativo.
             <div className='row justify-content-end'>
                 <div className="py-3 px-4 px-md-5 mb-3 col-lg-6 col-xl-7 order-2 order-md-1">
                     <Link className="btn btn-xl p-2 m-2" to="/" style={{ width: "200px" }}>
