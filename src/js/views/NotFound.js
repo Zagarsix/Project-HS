7179c283284b739f8ae8a83f4c931aeb3ab769e3
Zagarsix <claudk1006@gmail.com>
2024-10-04 23:51:28 -0300

    edit content NotFound

--- src/js/views/NotFound.js
+++ src/js/views/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (<>
         <main className="flex-shrink-0" id="notFound">
             <h1 className="site-heading fw-bolder px-4 mx-4 py-1 my-1">
-                <span style={{color:"#ff8800"}}>Error 404: </span> <span style={{ color: "#0e408f" }}> Página no disponible.</span>
+                <span style={{ color: "#ff8800" }}>Error 404: </span> <span style={{ color: "#0e408f" }}> Página no disponible.</span>
             </h1>
             <div className='row justify-content-center'>
                 <div className="py-3 px-4 px-md-5 mb-3 col-lg-6 col-xl-7 order-2 order-md-1">
@@ -16,8 +16,8 @@ const NotFound = () => {
                         Intenta volver al inicio de <span><strong>Colegio Virtual Alianza.</strong></span><br />
                     </p>
                     <Link className='link-primary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover fw-bolder p-2 m-2' to="/">
-                        <i className="fa-solid fa-book-open-reader fa-3x" style={{color:"#0e408f"}}></i></Link>
-                    <Link className="btn btn-xl p-2 m-2" to="/" style={{width:"200px"}}>
+                        <i className="fa-solid fa-book-open-reader fa-3x" style={{ color: "#0e408f" }}></i></Link>
+                    <Link className="btn btn-xl p-2 m-2" to="/" style={{ width: "200px" }}>
                         Inicio
                     </Link>
                 </div>
